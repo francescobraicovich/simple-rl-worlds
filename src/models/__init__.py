440b5b3dc04a4b17785ab4a71fd5f92ed25d8983
francescobraicovich <francesco.braicovich@gmail.com>
2025-07-12 19:36:07 +0200

    refactor: update model imports and exports in __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,18 +1,10 @@
 # models/__init__.py
-from .vit import ViT
-from .encoder_decoder import StandardEncoderDecoder
-from .jepa import JEPA
-from .cnn import CNNEncoder
-from .mlp import MLPEncoder
-from .decoder import StateDecoder
-from .encoder_decoder_jepa_style import EncoderDecoderJEPAStyle
+from .encoder import VideoViT
+from .predictor import LatentDynamicsPredictor
+from .decoder import HybridConvTransformerDecoder
 
 __all__ = [
-    "ViT",
-    "StandardEncoderDecoder",
-    "JEPA",
-    "CNNEncoder",
-    "MLPEncoder",
-    "StateDecoder",
-    "EncoderDecoderJEPAStyle"
+    "VideoViT",
+    "LatentDynamicsPredictor", 
+    "HybridConvTransformerDecoder"
 ]
