74cf55272160e01a5ad17f563208a893393ffd8f
francescobraicovich <francesco.braicovich@gmail.com>
2025-07-12 23:32:57 +0200

    feat: include RewardPredictor in the module's __all__ export list

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,9 +2,11 @@
 from .encoder import VideoViT
 from .predictor import LatentDynamicsPredictor
 from .decoder import HybridConvTransformerDecoder
+from .reward_predictor import RewardPredictor
 
 __all__ = [
     "VideoViT",
     "LatentDynamicsPredictor", 
-    "HybridConvTransformerDecoder"
+    "HybridConvTransformerDecoder",
+    "RewardPredictor"
 ]
