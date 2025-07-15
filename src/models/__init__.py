641a7eb97312135bb27738f8b97795677cc1fa93
Vittorio Garavelli <garavelliv04@gmail.com>
2025-07-15 17:45:44 +0200

    feat: add ConvEncoder to model exports for enhanced functionality

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,12 @@
 # models/__init__.py
-from .encoder import VideoViT
+from .encoder import VideoViT, ConvEncoder
 from .predictor import LatentDynamicsPredictor
 from .decoder import HybridConvTransformerDecoder
 from .reward_predictor import RewardPredictor
 
 __all__ = [
     "VideoViT",
+    "ConvEncoder",
     "LatentDynamicsPredictor", 
     "HybridConvTransformerDecoder",
     "RewardPredictor"
