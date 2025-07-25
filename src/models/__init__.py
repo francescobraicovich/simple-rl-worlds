ea2523b3f0ed9ef899beaa635f539b0cbe03ffeb
Vittorio Garavelli <garavelliv04@gmail.com>
2025-07-25 18:01:18 +0200

    feat: add PretrainedResNet18Encoder to model exports for enhanced architecture options

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 # models/__init__.py
-from .encoder import VideoViT, ConvEncoder
+from .encoder import VideoViT, ConvEncoder, PretrainedResNet18Encoder
 from .predictor import LatentDynamicsPredictor, MLPHistoryPredictor
 from .decoder import HybridConvTransformerDecoder, ConvDecoder
 from .reward_predictor import RewardPredictor
@@ -7,6 +7,7 @@ from .reward_predictor import RewardPredictor
 __all__ = [
     "VideoViT",
     "ConvEncoder",
+    "PretrainedResNet18Encoder",
     "LatentDynamicsPredictor",
     "MLPHistoryPredictor",
     "HybridConvTransformerDecoder",
